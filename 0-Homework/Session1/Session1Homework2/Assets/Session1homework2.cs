160f7f99fcd01159077f649c329b2dc4c2f5f9fa
xiayan.He <ucbqebx@ucl.ac.uk>
2020-11-05 22:28:59 +0000

    Homework/Session1

---
+++ 0-Homework/Session1/Session1Homework2/Assets/Session1homework2.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+public class Session1homework2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    void Start()
+    {
+        //define key and value
+        Dictionary<string, string> Xman= new Dictionary<string, string>();
+
+        //add data
+        Xman.Add("Superman", "50");
+        Xman.Add("Batman", "50");
+        Xman.Add("Deadpool", "50");
+        
+        //select data
+        Debug.Log(Xman["Deadpool"]);
+        //count the length of data
+        Debug.Log(Xman.Count);
+        //remove selected data
+        Debug.Log("Superman");
+        //change the data name
+        Xman["Deadpool"] = "Jackson";
+
+        //foreach loop
+        foreach (var item in Xman) 
+        {
+            Debug.Log(item.Key + "" + item.Value);
+          
+        }
+        //search for data
+        bool isContains = Xman.ContainsValue("Batman");
+
+        //take key 
+        ICollection<string> keys = Xman.Keys;
+        foreach(string key in keys)
+        {
+            //using key to take value
+            string value = Xman[key];
+            Debug.Log($"{key}={value}");
+
+        }
+        
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
