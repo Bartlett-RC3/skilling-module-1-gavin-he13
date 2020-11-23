02caa567e2c8c9bcbf411d49680d53dd34b9d93c
xiayan.He <ucbqebx@ucl.ac.uk>
2020-11-23 19:51:13 +0000

    Update Session 4 school work

---
+++ 0-Homework/Session4/New Unity Project (1)/Assets/Script/Session4.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Session4 : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    IEnumerator createPrefabsCoroutine;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        createPrefabsCoroutine = ColorPrefabs();
+        //calling  or using a corouutine
+        StartCoroutine(createPrefabsCoroutine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IEnumerator ColorPrefabs()
+        {
+            while (true)
+            {
+                //action the coroutines(create and drop the column)
+                Vector3 dropPosition = new Vector3(Random.Range(-5.0f, 12.0f), Random.Range(3.0f, 14.0f), Random.Range(-7.0f, 6.0f));
+                Quaternion dropRotation = new Quaternion(Random.Range(0, 90), Random.Range(0, 90), Random.Range(0, 90), 1);
+                Instantiate(cubePrefab, dropPosition, dropRotation);
+                yield return new WaitForSeconds(1);
+            }
+        }
+    }
+}
---
+++ 0-School/Session4/New Unity Project 2/Assets/Scripts/Session4.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Session4 : MonoBehaviour
+{
+    //variables 
+    public GameObject columnPrefab;
+    IEnumerator createPrefabsCoroutine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        createPrefabsCoroutine = DropPrefabs();
+        //calling  or using a corouutine
+        StartCoroutine(createPrefabsCoroutine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Time.time);
+        if (Time.time > 10)
+        {
+            //stop a specific coroutine
+            StopCoroutine(createPrefabsCoroutine);
+            //stop all coroutines
+            StopAllCoroutines();
+        }
+    }
+    //coroutines
+    IEnumerator DropPrefabs()
+    {
+        while (true)
+        {
+            //action the coroutines(create and drop the column)
+            Vector3 dropPosition = new Vector3(Random.Range(-5.0f, 12.0f), Random.Range(3.0f, 14.0f), Random.Range(-7.0f, 6.0f));
+            Quaternion dropRotation = new Quaternion(Random.Range(0, 90), Random.Range(0, 90), Random.Range(0, 90), 1);
+            Instantiate(columnPrefab, dropPosition, dropRotation);
+            yield return new WaitForSeconds(1);
+        }
+
+
+
+    }
+
+}
