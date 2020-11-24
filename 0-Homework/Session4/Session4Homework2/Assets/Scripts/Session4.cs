f536fe0010d9833dcf959f3b4ddf2a078456ed29
xiayan.He <ucbqebx@ucl.ac.uk>
2020-11-24 01:27:17 +0000

    Session4Homework

---
+++ 0-Homework/Session4/Session4Homework2/Assets/Scripts/Session4.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Session4 : MonoBehaviour
+{
+    //variables 
+    public GameObject cubePrefab;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //calling  or using a corouutine
+        StartCoroutine(cubeColorChange());
+    }
+
+    // Update is called once per frame
+    
+    //coroutines
+    IEnumerator cubeColorChange()
+    {
+        
+      for (int i = 0; i < 20; i++)
+        {
+            yield return new WaitForSeconds(1f);
+            Vector3 Position = new Vector3(Random.Range(-5.0f, 12.0f), Random.Range(3.0f, 14.0f), Random.Range(-7.0f, 6.0f));
+            Quaternion Rotation = new Quaternion(Random.Range(0, 90), Random.Range(0, 90), Random.Range(0, 90), 1);
+            Color color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            GameObject cubePrefabC = Instantiate(cubePrefab, Position, Rotation);
+            cubePrefabC.GetComponent<Renderer>().material.color = color;
+        }
+       
+    }
+}
---
+++ 0-Homework/Session4/Session4Homework2/Assets/Scripts/Session4homework2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Session4homework2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+             //move object forward
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.gameObject.transform.Translate(Vector3.up * 0.1f);
+
+        }
+        //move object backward
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.gameObject.transform.Translate(Vector3.down * 0.1f);
+
+        }
+        // check if an object is seeing another object using ray casting
+        // get the postion from where we are casting the ray
+        Vector3 rayCastPosition = this.gameObject.transform.position;
+
+        // create a ray in the direction way that want to casting
+        Vector3 rayCastDirection = this.gameObject.transform.TransformDirection(Vector3.up);
+
+        //create a variable that stores the info about what we are hitting
+        RaycastHit objectSeenByRay;
+
+        //cast the ray
+        if (Physics.Raycast(rayCastPosition, rayCastDirection, out objectSeenByRay))
+        {
+            Debug.Log("Object seen:" + objectSeenByRay.transform.name);
+            Debug.Log("Object position:" + objectSeenByRay.transform.ToString());
+            Debug.Log("Piont where ray is hitting the object:" + objectSeenByRay.point);
+            Destroy(objectSeenByRay.transform.gameObject);
+        }
+    }
+}
